c7c6b7c99e5d8692a968aa9bcb9c2d4162c80e0c
acnaman <acnadin01@gmail.com>
2020-04-23 08:30:25 +0900

    update build script

--- build/build.sh
+++ build/build.sh
@@ -1,12 +1,8 @@
-mkdir -p out/win_64
-mkdir -p out/mac_64
-mkdir -p out/linux_64
-
 # Mac 64bit
-GOOS=darwin GOARCH=amd64  go build -o out/mac_64/suddenlyMuscleTraining main.go
+GOOS=darwin GOARCH=amd64  go build -o out/mac_64/suddenlyMuscleTraining
 
 # Windows 64bit
-GOOS=windows GOARCH=amd64 go build -o out/win_64/suddenlyMuscleTraining.exe main.go
+GOOS=windows GOARCH=amd64 go build -o out/win_64/suddenlyMuscleTraining.exe
 
 # Linux 64bit
-GOOS=linux GOARCH=amd64 go build -o out/linux_64/suddenlyMuscleTraining main.go
+GOOS=linux GOARCH=amd64 go build -o out/linux_64/suddenlyMuscleTraining
