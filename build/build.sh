66ab3970000eb634e81422221914517f1919d3b0
acnaman <acnadin01@gmail.com>
2020-05-03 14:44:39 +0900

    GUI windows build

--- build/build.sh
+++ build/build.sh
@@ -2,7 +2,8 @@
 GOOS=darwin GOARCH=amd64  go build -o out/mac_64/suddenlyMuscleTraining
 
 # Windows 64bit
-GOOS=windows GOARCH=amd64 go build -o out/win_64/suddenlyMuscleTraining.exe
+CC=x86_64-w64-mingw32-gcc CGO_ENABLED=1 GOOS=windows GOARCH=amd64 go build -o out/win_64/suddenlyMuscleTraining.exe
 
 # Linux 64bit
-GOOS=linux GOARCH=amd64 go build -o out/linux_64/suddenlyMuscleTraining
+#CC=gcc CGO_ENABLED=1 GOOS=linux GOARCH=amd64 go build -o out/linux_64/suddenlyMuscleTraining
+
