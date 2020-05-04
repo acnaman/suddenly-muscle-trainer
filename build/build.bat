373c5761d146293b826a3cab537c81f8731b2396
acnaman <acnadin01@gmail.com>
2020-05-04 09:28:34 +0900

    update build script

--- build/build.bat
+++ build/build.bat
@@ -1,8 +1,11 @@
-# Mac 64bit
-GOOS=darwin GOARCH=amd64  go build -o out/mac_64/suddenlyMuscleTraining
+ren Mac 64bit
+GOOS=darwin GOARCH=amd64 go build -o out/mac_64/suddenlyMuscleTraining ./runtime
+GOOS=darwin GOARCH=amd64 go build -o out/mac_64/MTSetting ./gui
 
-# Windows 64bit
-GOOS=windows GOARCH=amd64 go build -o out/win_64/suddenlyMuscleTraining.exe
+rem Windows 64bit
+CC=x86_64-w64-mingw32-gcc CGO_ENABLED=1 GOOS=windows GOARCH=amd64 go build -o out/win_64/suddenlyMuscleTraining.exe ./runtime
+CC=x86_64-w64-mingw32-gcc CGO_ENABLED=1 GOOS=windows GOARCH=amd64 go build -o out/win_64/MTSetting.exe ./gui
 
-# Linux 64bit
-GOOS=linux GOARCH=amd64 go build -o out/linux_64/suddenlyMuscleTraining
+rem Linux 64bit
+rem CC=gcc CGO_ENABLED=1 GOOS=linux GOARCH=amd64 go build -o out/linux_64/suddenlyMuscleTraining
+
--- build/build.sh
+++ build/build.sh
@@ -1,8 +1,10 @@
 # Mac 64bit
-GOOS=darwin GOARCH=amd64  go build -o out/mac_64/suddenlyMuscleTraining
+GOOS=darwin GOARCH=amd64 go build -o out/mac_64/suddenlyMuscleTraining ./runtime
+GOOS=darwin GOARCH=amd64 go build -o out/mac_64/MTSetting ./gui
 
 # Windows 64bit
-CC=x86_64-w64-mingw32-gcc CGO_ENABLED=1 GOOS=windows GOARCH=amd64 go build -o out/win_64/suddenlyMuscleTraining.exe
+CC=x86_64-w64-mingw32-gcc CGO_ENABLED=1 GOOS=windows GOARCH=amd64 go build -o out/win_64/suddenlyMuscleTraining.exe ./runtime
+CC=x86_64-w64-mingw32-gcc CGO_ENABLED=1 GOOS=windows GOARCH=amd64 go build -o out/win_64/MTSetting.exe ./gui
 
 # Linux 64bit
 #CC=gcc CGO_ENABLED=1 GOOS=linux GOARCH=amd64 go build -o out/linux_64/suddenlyMuscleTraining
